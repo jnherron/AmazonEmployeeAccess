8a8d9c6fa5917adb8b1e6186a18c3630f71396f8
Jamie Herron <jnherron3@gmail.com>
2025-10-09 10:18:00 -0600

    Logistic Regression
    Performed Logistic Regression
    Updated README

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -6,17 +6,19 @@ library(tidymodels)
 
 
 
-# Upload Data
+# Upload Data ------------------------------------------------------------------
 train_data <- vroom("train.csv")
 test_data <- vroom("test.csv")
 
-# EDA
+# EDA --------------------------------------------------------------------------
 DataExplorer::plot_intro(train_data)         # Need to change continuous variable to discrete
 DataExplorer::plot_correlation(train_data)
 DataExplorer::plot_missing(train_data)       # None!!!! :)
 
 
-# Recipe
+# Recipe -----------------------------------------------------------------------
+train_data$ACTION <- as.factor(train_data$ACTION)
+
 my_recipe <- recipe(ACTION~., data=train_data) %>%
   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
   step_other(all_nominal_predictors(), threshold = .001) %>%
@@ -28,6 +30,23 @@ baked <- bake(prep, new_data = train_data)
 
 
 
+# Logistic Regression ----------------------------------------------------------
+logreg_mod <- logistic_reg() %>%
+  set_engine("glm")
+
+# Put into a workflow
+logreg_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(logreg_mod) %>%
+  fit(data=train_data)
+
+# Make Predictions
+logreg_preds <- predict(logreg_wf, new_data=test_data, type="prob")
+
+# Format for Kaggle
+kag_sub <- data.frame(id = test_data$id,
+                      ACTION = logreg_preds$.pred_1)
+vroom_write(x=kag_sub, file="./LogregPreds.csv", delim=",")
 
 
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ AmazonAnalysis.R
 - Loads in data
 - Basic EDA
 - Feature engineering / creates recipe
+- Performs Logistic Regression
